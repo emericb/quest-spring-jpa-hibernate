ecc1d7d4362d169aa7c2c367d24d4b9fc91b10bb
Emeric Bous <38536419+emericb@users.noreply.github.com>
2023-06-26 16:53:54 +0200

    challenge

--- src/main/java/com/wildcodeschool/wildandwizard/controller/SchoolController.java
+++ src/main/java/com/wildcodeschool/wildandwizard/controller/SchoolController.java
@@ -1,6 +1,8 @@
 package com.wildcodeschool.wildandwizard.controller;
 
 import com.wildcodeschool.wildandwizard.entity.School;
+import com.wildcodeschool.wildandwizard.repository.SchoolRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,15 +10,18 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Optional;
+
 @Controller
 public class SchoolController {
 
-    // TODO : get school repository by dependency injection
+    @Autowired
+    private SchoolRepository repository;
 
     @GetMapping("/schools")
     public String getAll(Model model) {
 
-        // TODO : find all schools
+        model.addAttribute("schools", repository.findAll());
 
         return "schools";
     }
@@ -25,7 +30,14 @@ public class SchoolController {
     public String getSchool(Model model,
                             @RequestParam(required = false) Long id) {
 
-        // TODO : find one school by id
+        School school = new School();
+        if (id != null) {
+            Optional<School> optionalWizard = repository.findById(id);
+            if (optionalWizard.isPresent()) {
+                school = optionalWizard.get();
+            }
+        }
+        model.addAttribute("school", school);
 
         return "school";
     }
@@ -33,16 +45,14 @@ public class SchoolController {
     @PostMapping("/school")
     public String postSchool(@ModelAttribute School school) {
 
-        // TODO : create or update a school
-
+        repository.save(school);
         return "redirect:/schools";
     }
 
     @GetMapping("/school/delete")
     public String deleteSchool(@RequestParam Long id) {
-
-        // TODO : delete a school
-
+        
+        repository.deleteById(id);
         return "redirect:/schools";
     }
 }
--- src/main/java/com/wildcodeschool/wildandwizard/entity/School.java
+++ src/main/java/com/wildcodeschool/wildandwizard/entity/School.java
@@ -1,9 +1,13 @@
 package com.wildcodeschool.wildandwizard.entity;
 
-// TODO : update this entity
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 public class School {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private Long capacity;
